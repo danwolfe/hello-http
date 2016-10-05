4ef17c55ab0d84090c9594d5cbc276c4642e8280
Daniel Wolfe <Daniel Wolfe>
2016-10-06 00:01:54 +1100

    Added initial commit to hello_http repo
    # Conflicts:
    #	hello_http/hello_http.js

--- hello_http/hello_http.js
+++ hello_http/hello_http.js
@@ -2,6 +2,10 @@ var http = require('http');
 
 var server = http.createServer(function(req, res) {
   res.writeHead(200);
+<<<<<<< HEAD
   res.end('Hello Http!!!');
+=======
+  res.end('Hello Http');
+>>>>>>> a61a817... Added initial commit to hello_http repo
 });
 server.listen(8080);
