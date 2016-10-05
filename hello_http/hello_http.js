45aab35bc8c413ac67a37cb82c464a7c7c4d026e
Daniel Wolfe <Daniel Wolfe>
2016-10-06 00:38:15 +1100

    Resolved merge confilcts between local hello-http and remote hello-http branch

--- hello_http/hello_http.js
+++ hello_http/hello_http.js
@@ -2,10 +2,6 @@ var http = require('http');
 
 var server = http.createServer(function(req, res) {
   res.writeHead(200);
-<<<<<<< HEAD
   res.end('Hello Http!!!');
-=======
-  res.end('Hello Http');
->>>>>>> a61a817... Added initial commit to hello_http repo
 });
 server.listen(8080);
